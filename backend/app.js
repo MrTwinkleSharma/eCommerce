64a0aac6664d069cd09a42775f59a3c74859e246
Twinkle Sharma <smtwinkle451@gmail.com>
2021-07-05 01:10:28 +0530

    Connecting to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 require('dotenv/config')
 const morgan = require('morgan')
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || '5000'
 const app = express();
@@ -11,7 +12,13 @@ app.get('/', (req, res, next)=>{
     res.send('Get Request on HOME PAGE of Backend!');
 })
 
-app.listen(PORT, ()=>{
-    console.log("Server is Listening on ", PORT);
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongodbcluster.mcnv5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+                { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true})
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Database connection is Ready and Server is Listening on Port ", PORT);
+    })
 })
-
+.catch((err)=>{
+    console.log("A error has been occured while connecting to database!");    
+})
