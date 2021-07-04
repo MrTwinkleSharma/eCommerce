c854a3608d420cc5723fe443d25bcd4d27d654e6
Twinkle Sharma <smtwinkle451@gmail.com>
2021-07-05 00:16:36 +0530

    Server Running on 5000

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+//3rd Party Modules
+const express = require('express');
+
+const PORT = process.env.PORT || '5000'
+const app = express();
+
+app.get('/', (req, res, next)=>{
+    res.send('Get Request on HOME PAGE of Backend!');
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server is Listening on ", PORT);
+})
+
